773a8ef731154415fd9dcbe0365e28ea0a874cd2
Brena Barradas <brenaraiaracb@gmail.com>
2025-08-11 17:03:41 -0300

    ajuste no comando da questão

--- src/Exercicios_Aula3/Lista2_Q11_ReajusteSalarial.java
+++ src/Exercicios_Aula3/Lista2_Q11_ReajusteSalarial.java
@@ -3,6 +3,18 @@ package Exercicios_Aula3;
 import java.util.Scanner;
 
 public class Lista2_Q11_ReajusteSalarial {
+    // As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.
+    //Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+    //salários até R$ 280,00 (incluindo) : de 20%
+    //salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+    //salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+    //salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser
+    //realizado, informe na tela:
+    //o salário antes do reajuste;
+    //o percentual de aumento aplicado;
+    //o valor do aumento;
+    //o novo salário, após o aumento.
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double salarioInicial;
